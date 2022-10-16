e424de8e588a8196853f4579983ad23ac15daad2
Lorenzo Curreli <lorenzocurreli@iMac-di-Lorenzo.local>
2022-10-16 10:45:55 +0200

    add routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+// import { Layout } from './components/Layout'
 
-function App() {
+export function App() {
   return (
-    <div>
-      <h1>Prova</h1>
-    </div>
-  );
+    <Routes>
+      <Route path="/" element={<div>This Is The Homepage</div>}/>
+      <Route path="me" element={<div>Pagina personale </div>} />
+      <Route path="me/stories" element={<div>I tuoi post</div>} />
+      <Route path="me/saved" element={<div>I tuoi post salvati</div>} />
+    </Routes>
+  )
 }
-
-export default App;
