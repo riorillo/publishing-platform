4a39684b93e1100b916dd8b2a8b6f54623f17111
Alessandro <a.sgherri92@gmail.com>
2022-11-08 20:31:47 +0100

    add logic for saved icon, add fetch for saved posts

--- client/src/components/Post/index.tsx
+++ client/src/components/Post/index.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Card from "@mui/material/Card";
 import { CardActionArea } from "@mui/material";
 import { Box } from "@mui/system";
@@ -8,39 +8,71 @@ import { Header } from "./Header";
 import { CardImage } from "./CardImage";
 import { CardFooter } from "./CardFooter";
 import { FooterIcons } from "./FooterIcons";
-import { Article } from "../SavedPost/mockArticle"
+import { Article } from "../SavedPost/mockArticle";
+import { UserContext, UserContextType } from "../../utils/context";
+import axios from "axios";
+import debounce from "lodash.debounce";
 
 type Props = {
-  article: Article
-}
+  article: Article;
+  removePostFromList?(articleId: string): void;
+  checkIfSaved?(articleId: string): boolean;
+};
 
-export default function Post({ article }: Props) {
+export default function Post({
+  article,
+  removePostFromList,
+  checkIfSaved,
+}: Props) {
+  const user = useContext<UserContextType>(UserContext);
+  const [saved, setSaved] = useState(false);
+
+  useEffect(() => {
+    if(checkIfSaved){
+      const isSaved = checkIfSaved(article.id);
+      setSaved(isSaved)
+    }
+  }, []);
+
+  async function handleSavePost() {
+    const savedPost = await axios.post(
+      "http://localhost:3001/api/saved",
+      {
+        userId: user.id,
+        postId: article.id,
+      },
+      {
+        headers: {
+          token: `Bearer ${user.accessToken}`,
+        },
+      }
+    );
+    if (savedPost.data.count && removePostFromList)
+      removePostFromList(article.id);
+  }
+
+  const debouncedOnChange = debounce(handleSavePost, 400);
+
   return (
-    <Card sx={styles.cardContainer}>
-      <Header
-        username={article.username}
-        userImage={article.userImage}
-        publishedAt={article.publishedAt}
-      />
-      <CardActionArea sx={styles.flex}>
-        <CardHeader title={article.title} description={article.description} />
-        <CardImage imageUrl={article.imageUrl} />
-      </CardActionArea>
-      <Box sx={styles.FooterContainer}>
-        <CardFooter tag={article.topic} readingTime={article.readingTime} />
-        <FooterIcons />
-      </Box>
-    </Card>
+    <>
+      <Card sx={styles.cardContainer}>
+        <Header
+          username={article.username}
+          userImage={article.userImage}
+          publishedAt={article.publishedAt}
+        />
+        <CardActionArea sx={styles.flex}>
+          <CardHeader title={article.title} description={article.description} />
+          <CardImage imageUrl={article.imageUrl} />
+        </CardActionArea>
+        <Box sx={styles.FooterContainer}>
+          <CardFooter tag={article.topic} readingTime={article.readingTime} />
+          <FooterIcons
+            checkSavedIcon={saved}
+            handleSavePost={debouncedOnChange}
+          />
+        </Box>
+      </Card>
+    </>
   );
 }
-
-// const oggetto = {
-//   userImage: "https://miro.medium.com/1*W0wM9xIeeIR3_Oo0E_thaA.png",
-//   username: "Alex",
-//   publishedAt: "20/10/2022",
-//   title: "Ciccio si è fatto grande",
-//   description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. A architecto commodi ut nihil non. Magni aspernatur repellat, dicta quibusdam voluptatibus numquam velit officia quam sed et corrupti doloremque quas quos.",
-//   imageUrl: "https://miro.medium.com/1*W0wM9xIeeIR3_Oo0E_thaA.png",
-//   readingTime: "5min",
-//   tag: "Non so",
-// };
--- client/src/components/SavedPost/index.tsx
+++ client/src/components/SavedPost/index.tsx
@@ -1,28 +1,85 @@
 import { Box, Typography } from "@mui/material";
-import { SyntheticEvent, useEffect, useState } from "react";
-import { Article } from "./mockArticle";
+import { SyntheticEvent, useContext, useEffect, useState } from "react";
 import PostFilter from "./PostFilter";
 import LayoutStyle from "./style";
-import { exampleArray } from "./mockArticle";
-import {exampleTabs} from "./mockArticle"
+import axios from "axios";
+import { UserContext, UserContextType } from "../../utils/context";
+
+interface SavedPostList {
+  id: string;
+  content: string;
+  contentFormatted: string;
+  image: string;
+  topic: string[];
+  title: string;
+  createAt: Date;
+  updateAt: Date;
+  authorId: string;
+}
 
 export default function SavedPost() {
   const [visualized, setVisualized] = useState("All");
-  const [visualizedList, setVisualizedList] = useState<Article[] | undefined>(
-    exampleArray
-  );
+  const [visualizedList, setVisualizedList] = useState<any>([]);
+  const [savedPosts, setSavedPosts] = useState<SavedPostList[]>([]);
+  const [topics, setTopics] = useState<string[]>([]);
+  const user = useContext<UserContextType>(UserContext);
+
+  function removePostFromList(articleId: any): any {
+    setSavedPosts((prev) => prev.filter((ele) => ele.id !== articleId));
+    const nextVisualizedList = savedPosts.filter((ele: any) =>
+      ele.topic.includes(visualized)
+    );
+    setVisualizedList(nextVisualizedList);
+  }
+
+  //function to verify if a post is saved or not
+  function checkIfSaved(articleId: string): boolean {
+    let isSaved = false;
+    savedPosts.forEach((ele) => {
+      if (ele.id === articleId) isSaved = true;
+    });
+    return isSaved;
+  }
+
+  useEffect(() => {
+    const fetchSaved = async () => {
+      if (!user || !user.id) return;
+      try {
+        const fetchedSavedPost = await axios.get(
+          `http://localhost:3001/api/saved/${user.id}`,
+          {
+            headers: {
+              token: `Bearer ${user.accessToken}`,
+            },
+          }
+        );
+        const topicList: string[] = [];
+        fetchedSavedPost.data.map((ele: SavedPostList) =>
+          ele.topic.forEach((innerEle: string) => {
+            if (!topicList.includes(innerEle)) topicList.push(innerEle);
+          })
+        );
+        setTopics(topicList);
+
+        setSavedPosts(fetchedSavedPost.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchSaved();
+  }, [user]);
 
   // Visualize the All filter or the selected one
   useEffect(() => {
     if (visualized === "All") {
-      setVisualizedList(exampleArray);
+      setVisualizedList(savedPosts);
     } else {
-      const nextVisualizedList = exampleArray.filter(
-        (ele) => ele.topic === visualized
+      const nextVisualizedList = savedPosts.filter((ele: any) =>
+        ele.topic.includes(visualized)
       );
       setVisualizedList(nextVisualizedList);
     }
-  }, [visualized]);
+  }, [visualized, savedPosts]);
 
   const onChange = (e: SyntheticEvent, newValue: string): void => {
     setVisualized(newValue);
@@ -37,8 +94,9 @@ export default function SavedPost() {
         visualizedList={visualizedList}
         visualized={visualized}
         handleChange={onChange}
-        topicList={exampleTabs}
-        postList={exampleArray}
+        topicList={topics}
+        removePostFromList={removePostFromList}
+        checkIfSaved={checkIfSaved}
       />
     </Box>
   );
